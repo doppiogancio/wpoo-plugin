691dd629a9f837dadbed9a693a78b84ede35b632
Fabrizio Gargiulo <fabrizio.gargiulo@gmail.com>
2021-03-22 12:47:44 +0100

    CS

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 cs-check:
-	php vendor/bin/phpcs ./src
+	php vendor/bin/phpcs --standard=PSR12 --standard=ruleset.xml src/
 
 cs-fix:
-	php vendor/bin/phpcbf ./src
+	php vendor/bin/phpcbf --standard=PSR12 --standard=ruleset.xml src/
--- src/XmlTransformer/Plugin.php
+++ src/XmlTransformer/Plugin.php
@@ -6,18 +6,26 @@ use WPOO\Plugin\Plugin as BasePlugin;
 
 class Plugin extends BasePlugin
 {
-    protected function defineAdminHooks()
+    protected function defineAdminHooks(): void
     {
         parent::defineAdminHooks();
 
         $pluginAdmin = new PluginAdmin($this);
 
-        $this->loader->addAction('wp_ajax_nopriv_start_script', [$pluginAdmin, 'startXmlTransformScript']);
-        $this->loader->addAction('wp_ajax_start_script', [$pluginAdmin, 'startXmlTransformScript']);
+        $this->loader->addAction(
+            'wp_ajax_nopriv_start_script',
+            [$pluginAdmin, 'startXmlTransformScript']
+        );
+
+        $this->loader->addAction(
+            'wp_ajax_start_script',
+            [$pluginAdmin, 'startXmlTransformScript']
+        );
 
         // Add admin page
         $this->loader->addAction(
-            'admin_menu', static function () use ($pluginAdmin) {
+            'admin_menu',
+            static function () use ($pluginAdmin) {
                 add_menu_page(
                     'XML Transformer',
                     'XML Transformer',
@@ -29,7 +37,8 @@ class Plugin extends BasePlugin
         );
 
         $this->loader->addAction(
-            'admin_init', static function () use ($pluginAdmin) {
+            'admin_init',
+            static function () use ($pluginAdmin) {
                 $pluginAdmin->registerSetting();
             }
         );
